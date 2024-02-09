8f4527dde050880a0844c8aa0e494c495f045103
Ali <2021170338@cis.asu.edu.eg>
2024-02-09 20:12:20 +0200

    solved problems with adding and editing products

--- Controllers/admin.js
+++ Controllers/admin.js
@@ -1,4 +1,5 @@
 const Product = require('../Models/products');
+const { validationResult } = require('express-validator');
 
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/add-product', {title : "Add Product", path : '/admin/add-product', product: {id: null}, auth: (req.session.user? 1: 0), verified: ((req.session.user && req.session.user.verified)? 1 : 0)});
@@ -20,7 +21,7 @@ exports.deleteProduct = (req, res) => {
 exports.getEdit = (req, res) => {
     Product.findById(req.params.prodId)
     .then(product => {
-        if(!product || !req.session.user || product.userId != req.session.user._id) return res.redirect('/admin/products');
+        if(!product || !req.session.user || product.userId.toString() != req.session.user._id.toString()) return res.redirect('/admin/products');
         res.render('admin/add-product', {title: 'Edit Product', path: '/admin/products', product: product, auth: (req.session.user? 1: 0), verified: ((req.session.user && req.session.user.verified)? 1 : 0)})
     })
     .catch(err => console.log(err));
@@ -29,13 +30,26 @@ exports.getEdit = (req, res) => {
 exports.postEdit = (req, res) => {
     Product.findById(req.params.prodId)
     .then(p => {
-        if(!p || !req.session.user || p.userId != req.session.user._id) return res.redirect('/admin/products');
+        if(!req.session.user || (p && p.userId.toString() != req.session.user._id.toString())) return res.redirect('/admin/products');
+        const error = validationResult(req);
+        if(!error.isEmpty()) {
+            return res.render('admin/add-product', {
+                title: 'Edit Product', 
+                path: '/admin/products', 
+                error: error.array()[0].msg,
+                product: p, 
+                auth: (req.session.user? 1: 0), 
+                verified: ((req.session.user && req.session.user.verified)? 1 : 0)
+            });
+        }
+        if(!p) p = new Product();
         p.title = req.body.title;
         p.price = req.body.price;
         p.quantity = req.body.quantity;
         p.description = req.body.description;
         p.imgUrl = req.body.imgUrl;
-        return p.save()
+        p.userId = req.session.user._id;
+        return p.save();
     })
     .then(() => res.redirect('/admin/products'))
     .catch(err => console.log(err));
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const { check } = require('express-validator');
 const adminController = require('../Controllers/admin');
 
 const route = express.Router();
@@ -7,9 +7,21 @@ const route = express.Router();
 
 route.get('/add-product/', adminController.getAddProduct);
 
-route.post('/add-product/', adminController.postEdit);
+route.post('/add-product/', 
+    [
+        check('quantity').isInt().withMessage('quantity must be a number'),
+        check('price').isFloat().withMessage('Price must be decimal number')
+    ], 
+    adminController.postEdit
+);
 
-route.post('/add-product/:prodId', adminController.postEdit);
+route.post('/add-product/:prodId', 
+    [
+        check('quantity').isInt().withMessage('quantity must be a number'),
+        check('price').isFloat().withMessage('Price must be decimal number')
+    ], 
+    adminController.postEdit
+);
 
 route.get('/products', adminController.getProducts);
 
